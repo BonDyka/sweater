4e21504cefa57ee7ca84ca8e9deac27064af6168
BonDyka <bondarew2507@gmail.com>
2019-10-11 09:15:46 +0300

    Add author field for Message (ManyToOne mapping)

--- src/main/java/loc/abondarev/sweater/controller/MainController.java
+++ src/main/java/loc/abondarev/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package loc.abondarev.sweater.controller;
 
 import loc.abondarev.sweater.domain.Message;
+import loc.abondarev.sweater.domain.User;
 import loc.abondarev.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,8 +30,10 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
 
         repo.save(message);
 
