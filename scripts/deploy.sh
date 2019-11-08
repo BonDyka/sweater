bc23999fc654db19cf82f159cbb09967c6c6c4dc
BonDyka <bondarew2507@gmail.com>
2019-11-08 12:19:28 +0300

    Deploy to server

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+mvn clean package
+
+echo 'Copy files...'
+
+scp -i ~/.ssh/authorized-keys.pem \
+        target/sweater-0.1.1.jar \
+        ubuntu@35.158.177.216:/home/ubuntu/
+
+echo 'Restart server...'
+
+ssh -tt -i ~/.ssh/authorized-keys.pem ubuntu@35.158.177.216 << EOF
+
+pgrep java | xargs kill -9
+nohup java -jar sweater-0.1.1.jar > log.txt &
+
+EOF
+
+echo 'Bye'
